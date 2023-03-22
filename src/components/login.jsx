334a5539eade59d40749d3a900325ca37d99288f
Leandro030404 <leandrobertainariver@gmail.com>
2023-03-22 15:05:56 -0300

    cambios menores de estilos al login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -7,11 +7,12 @@ const Login = () => {
             <h4 className='subtitle'>Ingrese su usuario y contraseña</h4>
             <div className="input-container">
                 <label htmlFor="user">Usuario</label>
-                <input type="text" name='user'/>
+                <input type="text" name='user' />
             </div>
             <div className="input-container">
                 <label htmlFor="password">Contraseña</label>
-                <input type="password" name='password'/>
+                <input type="password" name='password' />
+
             </div>
             <div className="btn-container">
                 <button className='btn'>Ingresar</button>
