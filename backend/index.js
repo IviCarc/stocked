f48d01d09b19009e4a7c181f403eed35d526ecfb
Leandro030404 <leandrobertainariver@gmail.com>
2023-04-25 12:06:09 -0300

    backend con mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
-var cors = require("cors");
-var express = require('express')
-var app = express()
 
-app.use(cors);
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-  })
+app.use(cors())
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
---
+++ backend/models/models.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+// const ProductosSchema = new Schema({
+//     producto : String,
+//     precio : Number,
+//     descripcion : String,
+//     categoria : String,
+//     cantidadDisponible : Number,
+//     imagenes : Buffer
+// })
+
+// const CategoriaSchema  = new Schema ({
+//     categoria : String,
+//     productos : [ProductosSchema]
+// })
+
+// const Producto = mongoose.model("Producto", ProductosSchema);
+// const Categoria = mongoose.model("Categoria", CategoriaSchema);
+
+// module.exports = {Categoria, Producto}
