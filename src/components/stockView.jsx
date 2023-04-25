2b8d7b7b7097c9e081c1bef514cbd3297b8b451a
Leandro030404 <leandrobertainariver@gmail.com>
2023-04-25 11:41:16 -0300

    cambios menores gap de stock

--- src/components/stockView.jsx
+++ src/components/stockView.jsx
@@ -38,6 +38,11 @@ const StockView = () => {
                 <Card></Card>
                 <Card></Card>
                 <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
             </div>
         </div>
 
