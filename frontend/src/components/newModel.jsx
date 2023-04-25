4befd152e00e3fcd2b9c57a9a2ad6f1f770e70e6
LeaMarconcini <103255964+LeaMarconcini@users.noreply.github.com>
2023-04-25 12:34:00 -0300

    inicio del createmodel

--- frontend/src/components/newModel.jsx
+++ frontend/src/components/newModel.jsx
@@ -1,9 +1,17 @@
+import "./newModel.css";
+import "./inputs.css";
 const NewModel = (props) => {
-    return (
-        <div className="newModel">
-            NEW MODEL
-        </div>
-    )
-}
+  return (
+    <div className="newModel">
+      <div className="cont">
+        <input type="text" className="input inputs" />
+        <input type="text" className="input inputs" />
+        <input type="text" className="input inputs" />
+        <input type="text" className="input inputs" />
+        <input type="text" className="input inputs" />
+      </div>
+    </div>
+  );
+};
 
 export default NewModel;
