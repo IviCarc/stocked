87748ac36b03655737a65308641bab9d424c0ea0
LeaMarconcini-Escuela <103255964+LeaMarconcini@users.noreply.github.com>
2023-05-23 11:59:28 -0300

    Actualizacion de producto

--- frontend/src/components/product.jsx
+++ frontend/src/components/product.jsx
@@ -11,7 +11,26 @@ const Product = (props) => {
           if (obj._id == location) {
             return (
              <>
-             {/* ACA HACER LA PAGINA DEL PRODUCTO CON PROPS */}
+             {
+            <div>
+              <div className="about-producto">
+              <div className="img-product">
+                <img src={require("../imgs/libro.avif")} alt="adawd" />
+                {/* props.img */}
+              </div>
+              <div className="price-producto">
+                <p><b>${props.precio}</b> </p>
+                <hr />
+                <p><b>Editar</b> </p>
+                <hr />
+                <p><b>Eliminar</b> </p>  
+              </div>
+             </div>
+              <div className="info-producto">
+
+              </div>
+             </div>
+             }
                 {obj.producto}
              </>
             )
