4138e5d12ab57d7a23a6934c85114b31785cca94
ivan-pc-atras <kovachivanagustin@gmail.com>
2023-05-31 11:59:17 -0300

    estilos

--- frontend/src/components/newProduct.jsx
+++ frontend/src/components/newProduct.jsx
@@ -1,9 +1,10 @@
-import {useState} from "react"
+import { useState } from "react"
 import './inputs.css'
+import './newProduct.css'
 
 const NewProduct = () => {
 
-    const [producto ,setProducto] = useState(null)
+    const [producto, setProducto] = useState(null)
 
     const subirProducto = (e) => {
         console.log("ENVIADO")
@@ -11,30 +12,32 @@ const NewProduct = () => {
 
     return (
         <div className="new-product">
-            <div className="input-div">
-                <label htmlFor="producto">Nombre del producto:</label>
-                <input className='input' type="text" name="producto"/>
+            <div className="inputs-container">
+                <div className="input-div">
+                    <label htmlFor="producto">Nombre del producto:</label>
+                    <input className='input' type="text" name="producto" />
+                </div>
+                <div className="input-div">
+                    <label htmlFor="categoria">Categoría</label>
+                    <select name="categoria" id='categoria'>
+                        <option selected disabled>Categoría</option>
+                        <option value="lg">lg</option>
+                    </select>
+                </div>
+                <div className="input-div">
+                    <label htmlFor="producto">Cantidad del producto:</label>
+                    <input className='input' type="number" name="cantidad" />
+                </div>
+                <div className="input-div">
+                    <label htmlFor="precio">Precio:</label>
+                    <input className='input' type="number" name="precio" />
+                </div>
+                <div className="input-div">
+                    <label htmlFor="producto">Subir imagen:</label>
+                    <input className='' type="file" name="producto" />
+                </div>
+                <button className='btn' onClick={subirProducto}>Subir producto</button>
             </div>
-            <div className="input-div">
-                <label htmlFor="categoria">Categoría</label>
-                <select name="categoria" id='categoria'>
-                    <option selected disabled>Categoría</option>
-                    <option value="lg">lg</option>
-                </select>
-            </div>
-            <div className="input-div">
-                <label htmlFor="producto">Cantidad del producto:</label>
-                <input className='input' type="number" name="cantidad"/>
-            </div>
-            <div className="input-div">
-                <label htmlFor="precio">Precio:</label>
-                <input className='input' type="number" name="precio"/>
-            </div>
-            <div className="input-div">
-                <label htmlFor="producto">Subir imagen:</label>
-                <input className='' type="file" name="producto"/>
-            </div>
-            <button className='btn' onClick={subirProducto}>Subir producto</button>
         </div>
     )
 }
