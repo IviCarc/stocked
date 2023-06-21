802d94e1a7605bab79a6935cf7bd7c8fd4302fd6
maximo <avryux04@gmail.com>
2023-06-21 11:53:42 -0300

    cambios visuales a newProduct

--- frontend/src/components/newProduct.jsx
+++ frontend/src/components/newProduct.jsx
@@ -7,6 +7,7 @@ import axios from 'axios'
 
 const NewProduct = (props) => {
     const [listaCategorias, setListaCategorias] = useState(null);
+    const [res, setRes] = useState(false)
 
     const obtenerCategorias = async () => {
         // const categorias = await axios.get('http://localhost:5000/todas-categorias');
@@ -60,69 +61,83 @@ const NewProduct = (props) => {
         e.preventDefault();
 
         let data = {};
-
+        let res
+        
         for (const key in state) {
             if (key === "precio" || key == "cantidadDisponible") data[key] = parseInt(state[key].value);
             else data[key] = state[key].value;
         }
 
         try {
-
-            console.log("DATA", data)
-            await axios.post("http://192.168.2.155:5000/" + url, data, { headers: { "content-type": contentType } });
-
+            res = await axios.post("http://192.168.2.155:5000/" + url, data, { headers: { "content-type": contentType } });
         } catch (e) {
             console.log(e);
             return;
         }
+        setRes(true)
+
         // fetch('http://192.168.2.155:5000/todas-categorias')
     };
 
 
     return (
         <div className="new-product">
-            <div className="inputs-container">
-                <div className="input-div">
-                    <label htmlFor="producto" className='input-label'>Nombre del producto:</label>
-                    <input className='input' type="text" name="producto" onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)}/>
-                </div>
-                <div className="input-div">
-                    <label htmlFor="categoria" className='input-label'>Categoría</label>
-                    <div class="select" id='newproduct-select'>
-                        <select name="categoria" id="categoria" onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} >
-                            {/* <option selected disabled>Modelo</option>
+            <div className="new-product-inside-div">
+
+                <div className="inputs-container">
+                    <div className="inputs-container-separator">
+                        <div className="input-div">
+                            <label htmlFor="producto" className='input-label'>Nombre del producto:</label>
+                            <input className='input' type="text" name="producto" onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} />
+                        </div>
+                        <div className="input-div">
+                            <label htmlFor="categoria" className='input-label'>Categoría</label>
+                            <div class="select" id='newproduct-select'>
+                                <select name="categoria" id="categoria" onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} >
+                                    {/* <option selected disabled>Modelo</option>
                             <option value="libro">Libro</option>
                             <option value="auto">Auto</option> */}
 
-                            <option selected value='none'>Seleccione una categoria</option>
-                            {listaCategorias && listaCategorias.map((obj, i) => {
-                                return <option key={i} value={obj.categoria}>{obj.categoria}</option>
-                            })}
+                                    <option selected value='none'>Seleccione una categoria</option>
+                                    {listaCategorias && listaCategorias.map((obj, i) => {
+                                        return <option key={i} value={obj.categoria}>{obj.categoria}</option>
+                                    })}
 
-                        </select>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="input-div">
+                            <label htmlFor="descripcion" className='input-label'>Descripción del producto:</label>
+                            <input className='input' onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} type="text" name="descripcion" />
+                        </div>  
+                    </div>
+                    <div className="inputs-container-separator">
+                        <div className="input-div">
+                            <label htmlFor="cantidadDisponible" className='input-label'>Cantidad del producto:</label>
+                            <input className='input' onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} type="number" name="cantidadDisponible" />
+                        </div>
+                        <div className="input-div">
+                            <label htmlFor="precio" className='input-label'>Precio:</label>
+                            <input className='input' onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} type="number" name="precio" />
+                        </div>
+                        <div className="input-div">
+                            <label htmlFor="imagen" className='input-label'>Subir imagen:</label>
+                            <input className='' type="file" name="imagen" id="imagen" onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} />
+                            <div className='div-imagen'>
+                                <label htmlFor="imagen" className='input input-imagen' type="text" />
+                                <FontAwesomeIcon icon={faCloudArrowUp} className="cloudIcon " id='img-icon' />
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="input-div">
-                    <label htmlFor="descripcion" className='input-label'>Descripción del producto:</label>
-                    <input className='input' onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} type="text" name="descripcion" />
-                </div>
-                <div className="input-div">
-                    <label htmlFor="cantidadDisponible" className='input-label'>Cantidad del producto:</label>
-                    <input className='input' onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} type="number" name="cantidadDisponible" />
-                </div>
-                <div className="input-div">
-                    <label htmlFor="precio" className='input-label'>Precio:</label>
-                    <input className='input' onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)} type="number" name="precio" />
-                </div>
-                <div className="input-div">
-                    <label htmlFor="imagen" className='input-label'>Subir imagen:</label>
-                    <input className='' type="file" name="imagen" id="imagen" onChange={(e) => onChange(e, setNuevoProducto, nuevoProducto)}/>
-                    <div className='div-imagen'>
-                        <label htmlFor="imagen" className='input input-imagen' type="text" />
-                        <FontAwesomeIcon icon={faCloudArrowUp} className="cloudIcon " id='img-icon' />
-                    </div>
-                </div>
+
                 <button className='btn' id='send-btn' onClick={(e) => sendData(e, nuevoProducto, 'nuevo-producto', 'multipart/form-data')}>Subir producto</button>
+
+                {
+
+                    res && <p className='sended'>Producto enviado correctamente</p>
+                }
+
             </div>
         </div>
     )
