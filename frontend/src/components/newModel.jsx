aaae229b0e5831d07afb96410110196ec7592334
ivan-notebook <kovachivanagustin@gmail.com>
2023-08-10 20:06:07 -0300

    agregar y eliminar inputs finalizado

--- frontend/src/components/newModel.jsx
+++ frontend/src/components/newModel.jsx
@@ -11,8 +11,8 @@ import axios from 'axios'
 
 const NewModel = (props) => {
 
-    const [caracteristicas, setCaracteristicas] = useState([]);
-    
+    const [caracteristicas, setCaracteristicas] = useState([""]);
+
     const onChange = (e, setter, state) => {
         const inputName = e.target.name;
         const inputValue = e.target.value;
@@ -24,15 +24,6 @@ const NewModel = (props) => {
             },
         });
     };
-
-    const [nuevoModelo, setNuevoModelo] = useState({
-        producto: { value: "", valid: false },
-        descripcion: { value: "", valid: false },
-        precio: { value: "", valid: false },
-        categoria: { value: "", valid: false },
-        cantidadDisponible: { value: "", valid: false },
-        imagen: { value: "", valid: false },
-    });
 
     const sendData = async (e, state, url, contentType) => {
         e.preventDefault();
@@ -52,9 +43,23 @@ const NewModel = (props) => {
     };
 
     const agregarCaracteristica = (e) => {
-        console.log(caracteristicas);
-        setCaracteristicas(<input type="text" className="input inputs" placeholder="Característica" />);
-        console.log(caracteristicas);
+        // Al primer click escribe un 0, luego escribe el id correspondiente y así
+        // setCaracteristicas([...caracteristicas,caracteristicas.length ==  0 ? 0 : caracteristicas[caracteristicas.length - 1 ] + 1]);
+        setCaracteristicas([...caracteristicas, ""]);
+    }
+
+    const borrarCaracteristica = (i) => {
+        const temp = [...caracteristicas];
+        console.log(i)
+        temp.splice(i, 1);
+        setCaracteristicas(temp);
+    }
+
+    const actualizarCaracteristica = (e, i) => {
+        const temp = [...caracteristicas];
+        temp[i] = e.target.value
+        console.log(temp)
+        setCaracteristicas(temp)
     }
 
     return (
@@ -66,7 +71,20 @@ const NewModel = (props) => {
                     <input type="text" className="input inputs" placeholder="Nombre del modelo" />
 
                     {
-                        caracteristicas
+                        caracteristicas && caracteristicas.map((obj, i) => {
+                            return (
+                                <div className="trash" key={i} >
+                                    {/* RENDERIZA EL ICONO DE ELIMINAR SOLO SI SE AGREGÓ MAS DE UNA CARACTERISTICA */}
+                                    {caracteristicas.length > 1  && 
+
+                                    (<FontAwesomeIcon icon={faTrash} className="trashIcon" onClick={() => borrarCaracteristica(i)} listid={i} />)
+                                   
+                                    }
+                                    <input type="text" className="input inputs trashInput " placeholder="Caracteristica creada" value={obj}
+                                        onChange={(e) => actualizarCaracteristica(e, i)} />
+                                </div>
+                            )
+                        })
                     }
 
                     <div className="plus" >
@@ -74,19 +92,8 @@ const NewModel = (props) => {
                         <input type="submit" className="input inputs plusinput" onClick={agregarCaracteristica} value="Añadir Caracteristica" />
                     </div>
 
-                    <button type="button" className="btn" onClick={sendData}>Crear</button>
+                    <button id="boton" type="button" className="btn" onClick={sendData}>Crear</button>
 
-                    {/* <input type="text" className="input inputs" placeholder="Característica" /> */}
-
-                    {/* <div className="cluod" >
-                        <FontAwesomeIcon icon={faCloudArrowUp} className="cloudIcon" />
-                        <input type="submit" className="input inputs cloudInput " value="Subir foto" />
-                    </div> */}
-                    {/* 
-                    <div className="trash" >
-                        <FontAwesomeIcon icon={faTrash} className="trashIcon" />
-                        <input type="text" className="input inputs trashInput " placeholder="Caracteristica creada" />
-                    </div> */}
                 </div>
 
             </div>
