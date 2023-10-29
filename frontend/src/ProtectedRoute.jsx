aa8c63e6d22668bc71cf2a1415b600b96f02b362
ivan-linux <kovachivanagustin@gmail.com>
2023-10-29 17:25:28 -0300

    timeout ProtectedRou

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -6,16 +6,20 @@ const MySwal = withReactContent(Swal)
 
 const Loading = () => {
 
-    MySwal.fire({
-        position: 'center',
-        showSpinner: true,
-        showConfirmButton: false,
-        timer: 500,
-        title: 'Cargando...',
-        onBeforeOpen: () => {
-            Swal.showLoading()
-        },
-    })
+    setTimeout(() => {
+        MySwal.fire({
+            position: 'center',
+            showSpinner: true,
+            showConfirmButton: false,
+            timer: 500,
+            title: 'Cargando...',
+            onBeforeOpen: () => {
+                Swal.showLoading()
+            },
+        })
+    }, 500)
+
+    
 
     return (
         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '100%', height: '100%', backgroundColor: '#fff' }} className="loading-div">
